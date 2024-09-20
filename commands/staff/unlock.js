d777cc7159c2f42137df6442779e9f912b648f7c
PlinxyDev <hello@plinxy.me>
2024-09-20 09:27:19 +0100

    Added the /lock and /unlock commands

---
+++ commands/staff/unlock.js
@@ -0,0 +1,41 @@
+const {
+    SlashCommandBuilder,
+    EmbedBuilder,
+    PermissionsBitField,
+  } = require("discord.js");
+  const { color } = require("../../index.js");
+  
+  module.exports = {
+    data: new SlashCommandBuilder()
+      .setName("unlock")
+      .setDescription("Unlocks the current channel so anyone can talk."),
+  
+    run: async (client, interaction) => {
+      const embed = new EmbedBuilder();
+      const channel = interaction.channel;
+  
+      try {
+        await channel.permissionOverwrites.edit(
+          interaction.guild.roles.everyone,
+          {
+            [PermissionsBitField.Flags.SendMessages]: true,
+          }
+        );
+  
+        embed
+          .setTitle("<:earth:1286361875046731848> Channel Unlocked")
+          .setDescription("This channel has been unlocked by a staff member.")
+          .setTimestamp()
+          .setColor(color);
+  
+        await interaction.reply({ embeds: [embed] });
+      } catch (error) {
+        console.error(error);
+        interaction.reply({
+          content: "An error occurred while trying to lock the channel.",
+          ephemeral: true,
+        });
+      }
+    },
+  };
+  
